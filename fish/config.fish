092a0282bace05a233943d09f210e045988934a5
brendn <brendn@users.noreply.github.com>
2023-10-01 13:01:42 -0400

    init

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -g theme_color_scheme nord
+function fish_greeting
+	pokemon-colorscripts -r
+end
+cat ~/.cache/wal/sequences
---
+++ fish/functions/reload.fish
@@ -0,0 +1,4 @@
+function reload --wraps='~/.config/scripts/reload.sh' --description 'alias reload ~/.config/scripts/reload.sh'
+  ~/.config/scripts/reload.sh $argv
+        
+end
